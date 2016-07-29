d199ce4f475faebbdb024337b926b49a7104ce14
Jherson7 <jhersonaos7@gmail.com>
2016-07-28 20:39:11 -0600

    modifcacion tokens

--- Fase1_201346094/main.c
+++ Fase1_201346094/main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+//declaracion de prototipos de los metodos que voy a requerir
+void crearDisco(char * token);
+
 
 int main()
 {
-    printf("Hello world!\n");
+    int n=0;
+
+    char *token;
+
+    while(n==0)
+    {
+        printf("Ingrese comandos a Ejecutar!\n");
+        char comando[200]="";
+        fgets(comando,200,stdin);
+        const char s[2] = " ";
+        char *token;
+
+        token = strtok(comando, s);
+
+        while( token != NULL )
+        {
+            printf( " %s\n", token );
+            if(strcmp(token,"Mkdisk")==0 || strcmp(token,"mkdisk")==0){
+                    token = strtok(NULL, s);
+                    crearDisco(token);
+            }
+            token = strtok(NULL, s);
+        }
+
+    }
+
+
     return 0;
+}
+
+void crearDisco(char * token){
+printf("imprimo desde el metodo\n");
+const char s[2] = " ";
+    while( token != NULL )
+    {
+
+        printf( " %s\n", token );
+        token = strtok(NULL, s);
+
+    }
 }
